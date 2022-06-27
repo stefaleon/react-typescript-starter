48a8cf4612a74a35020b48dfe43f5cd971c97b3f
stefaleon <stefaleon@gmail.com>
2022-06-27 15:20:59 +0300

    Add a wildcard route for 404

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import NotFound from './pages/NotFound';
 import Home from './pages/Home';
 import Page1 from './pages/Page1';
 
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route index element={<Home />} />
         <Route path='page-1' element={<Page1 />}></Route>
+        <Route path='*' element={<NotFound />}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,5 @@
+function NotFound() {
+  return <div>404 - Cannot find this page...</div>;
+}
+
+export default NotFound;
