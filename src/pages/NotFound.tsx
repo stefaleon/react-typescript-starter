b1a2a1b25308b8a280b42361fde60e836624b7dc
stefaleon <stefaleon@gmail.com>
2022-06-27 16:00:40 +0300

    Display the location pathname for the unknown routes

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,14 @@
+import { useLocation } from "react-router-dom";
+
 function NotFound() {
-  return <div>404 - Cannot find this page...</div>;
+  const location = useLocation();
+
+  return (
+    <>
+      <div>404 - Cannot find this page...</div>
+      <div> {location.pathname}</div>
+    </>
+  );
 }
 
 export default NotFound;
